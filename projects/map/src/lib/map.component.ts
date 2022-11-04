f8abedecb96816e44560c011ea259d71051cbd0d
uweduesing <uwe.duesing@gmail.com>
2022-11-04 14:47:41 +1300

    initial stuff

---
+++ projects/map/src/lib/map.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-map',
+  template: `
+    <p>
+      map works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/map/src/lib/map.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MapComponent } from './map.component';
+
+
+
+@NgModule({
+  declarations: [
+    MapComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    MapComponent
+  ]
+})
+export class MapModule { }
