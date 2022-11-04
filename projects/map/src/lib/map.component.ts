610bb67c76f750ce37cdcd160ebaa4614f4c147b
uweduesing <uwe.duesing@gmail.com>
2022-11-04 15:48:37 +1300

    Added openlayers

--- projects/map/src/lib/map.component.ts
+++ projects/map/src/lib/map.component.ts
@@ -1,16 +1,39 @@
-import {Component, OnInit} from '@angular/core';
-
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import Map from 'ol/Map'
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import {fromLonLat} from "ol/proj";
 @Component({
   selector: 'lib-map',
   templateUrl: 'map.component.html',
   styleUrls: ['map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
   constructor() {
   }
 
   ngOnInit(): void {
+
+  }
+  ngAfterViewInit() {
+    this.createMap();
   }
 
+
+  public createMap = () => {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'map',
+      view: new View({
+        center: fromLonLat([175, -37]),
+        zoom: 7,
+      }),
+    });
+  }
 }
